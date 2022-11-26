ae78e68b276b20cf25d80a6f9c6d987b80c95b13
annelivali <anvali@ttu.ee>
2022-11-26 23:19:18 +0200

    * Added UnitTests for CurrentWeatherReport

--- src/main/test/java/UnitTests.java
+++ src/main/test/java/UnitTests.java
@@ -58,4 +58,31 @@ public class UnitTests {
         Assert.assertTrue(LogsAppender.returnLogList().size() > 0);
         Assert.assertEquals(errorMessage, LogsAppender.returnLogList().get(LogsAppender.returnLogList().size() - 1));
     }
+
+    @Test
+    public void testCurrentWeatherReportTemp() {
+        CurrentWeatherReport currentWeatherReport = new CurrentWeatherReport();
+        currentWeatherReport.setTemperature(277.15);
+        Assert.assertEquals(4, currentWeatherReport.getTemperature(), 0);
+        currentWeatherReport.setTemperature(279.15);
+        Assert.assertEquals(5, currentWeatherReport.getTemperature(), 0);
+    }
+
+    @Test
+    public void testCurrentWeatherReportHumidity() {
+        CurrentWeatherReport currentWeatherReport = new CurrentWeatherReport();
+        currentWeatherReport.setHumidity(60);
+        Assert.assertEquals(60, currentWeatherReport.getHumidity(), 0);
+        currentWeatherReport.setHumidity(90);
+        Assert.assertEquals(75, currentWeatherReport.getHumidity(), 0);
+    }
+    
+    @Test
+    public void testCurrentWeatherReportPreassure() {
+        CurrentWeatherReport currentWeatherReport = new CurrentWeatherReport();
+        currentWeatherReport.setPressure(1000);
+        Assert.assertEquals(1000, currentWeatherReport.getPressure(), 0);
+        currentWeatherReport.setPressure(1200);
+        Assert.assertEquals(1100, currentWeatherReport.getPressure(), 0);
+    }
 }
