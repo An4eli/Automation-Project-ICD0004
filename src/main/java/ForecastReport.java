99b7e019f6f2cfe55ae5890562b23e1ee5d3b129
annelivali <anvali@ttu.ee>
2022-11-30 23:32:16 +0200

    * Added functionality for ForecastReport class

--- src/main/java/ForecastReport.java
+++ src/main/java/ForecastReport.java
@@ -1,2 +1,24 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ForecastReport {
+    private String date;
+    private CurrentWeatherReport weather;
+    public ForecastReport(){
+    }
+
+    public String getDate() {
+        return date;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public CurrentWeatherReport getWeather() {
+        return weather;
+    }
+    public void setWeather(CurrentWeatherReport value) {
+
+        this.weather = value;
+    }
 }
